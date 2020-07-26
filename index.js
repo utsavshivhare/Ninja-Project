7d46b81cb6a98df2e873a2d57d8473e933e740f9
utsavshivhare <65752243+utsavshivhare@users.noreply.github.com>
2020-07-26 21:14:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// for smooth scroll but not working correct
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+
+
 $(document).ready(function () {
   
     //Navbar scroll hide effect code below
@@ -34,6 +43,49 @@ $(document).ready(function () {
     });
 
 
+
+    //Navbar hide on focus out
+    $(function() {
+      $(document).on("click scroll",function (event) {
+        $('.navbar-collapse').collapse('hide');
+      });
+    });
+
+
+// for photo
+var $wrap = $('.wrapper'),
+  $lEye = $('.eye'),
+  lFollowX = 5,
+  lFollowY = 10,
+  x = 0,
+  y = 0,
+  friction = 1 / 12;
+
+function animate() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+
+  $lEye.css({
+    '-webit-transform': 'translate(' + -x + 'px, ' + -y + 'px)',
+    '-moz-transform': 'translate(' + -x + 'px, ' + -y + 'px)',
+    'transform': 'translate(' + -x + 'px, ' + -y + 'px)'
   });
 
-  
+  $wrap.css({
+    'transform': 'translate(-50%, -50%) perspective(600px) rotateY(' + -x + 'deg) rotateX(' + y + 'deg)'
+  });
+  window.requestAnimationFrame(animate);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (12 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+animate();
+
+});
